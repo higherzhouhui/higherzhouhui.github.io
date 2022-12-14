115d39d3a784baebcdd4acb292a4d54270ff9c1c
zhouhui <782492184@qq.com>
2022-12-14 16:59:08 +0800

    test

---
+++ js/side-channel/side-channel.cd9fb761.js
@@ -0,0 +1,1 @@
+import{g as r}from"../get-intrinsic/get-intrinsic.5afea291.js";import{c as p}from"../call-bind/call-bind.89d60758.js";var e=r,t=p;e("%TypeError%");e("%WeakMap%",!0);e("%Map%",!0);t("WeakMap.prototype.get",!0);t("WeakMap.prototype.set",!0);t("WeakMap.prototype.has",!0);t("Map.prototype.get",!0);t("Map.prototype.set",!0);t("Map.prototype.has",!0);
